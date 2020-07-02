e279a8868b842dd6e1e2711c890de583465f8f70
Sean McDonald <Seanmichaelmcdonald@gmail.com>
2020-07-02 15:20:42 -0400

    Create cards.js

---
+++ cards.js
@@ -0,0 +1,31 @@
+//format for the cards --
+
+/*
+      <li>
+        <a href="#">
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/25480/numbers-01.jpg" alt="">
+          <p>Student #1</p>
+        </a>
+      </li>
+*/
+
+var cards = "";
+
+for(var i == 0; i < students.length; i++){
+  
+  var card = `<li>
+        <a href="#">
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/25480/numbers-01.jpg" alt="">
+          <p>Student #1</p>
+        </a>
+      </li>`;
+ 
+
+   cards += card;
+
+}
+
+var list = document.getElementById("gallery");
+
+list.innerHTML = cards;
+

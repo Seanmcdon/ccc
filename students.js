17ab7ffa2239de185edd8870dcdb0793997b6679
Sean McDonald <Seanmichaelmcdonald@gmail.com>
2020-07-02 15:13:38 -0400

    Create students.js
    student names and links

---
+++ students.js
@@ -0,0 +1,104 @@
+const students = [
+{ name: "Brian",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Cyneathia",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Daniel",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Isaiah",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Jaiden",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Ja'Naye",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Janiece",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Kanazja",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Mahkiah",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Makayla",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Mikal",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Na'Leah",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Nasir",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Peyton",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Raelyn",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Robert",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+},
+{ name: "Shamir",
+  picture: "",
+  python_trinket: "",
+  jsfiddle: "",
+  codepen: "" 
+}
+];
